aca3a9c47a730c417c78aff0f884c0f623e1c573
guangjh <guangjinhai@163.com>
2021-04-10 07:37:51 +0800

    test

--- hello/hello.go
+++ hello/hello.go
@@ -8,11 +8,28 @@ import (
 )
 
 func main() {
+	// singleGreeting()?
+	multipleGreeting()
+}
+
+func multipleGreeting() {
 	log.SetPrefix("greetings log: ")
 	log.SetFlags(0)
 
-	// message := greetings.Hello("kevin")
-	message, err := greetings.Hello("")
+	names := []string{"AAA", "BBB", "CCC"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
+
+func singleGreeting() {
+	log.SetPrefix("greetings log: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("kevin")
+	// message, err := greetings.Hello("")
 
 	if err != nil {
 		log.Fatal(err)
